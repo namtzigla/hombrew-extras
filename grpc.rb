ad58edc88249042e18bcb6cfd99ce8f1ed304aea
Florin STAN <namtzigla@users.noreply.github.com>
2016-06-14 16:26:52 -0600

    Update grpc.rb

--- grpc.rb
+++ grpc.rb
@@ -5,7 +5,7 @@ class Pjsip < Formula
   homepage 'http://grpc.io'
   url ' https://github.com/grpc/grpc/archive/release-0_14_1.tar.gz'
   sha256 '006cde82481d72f3490e7d933fe91a81bec3b30a6928251796ea47a0098267de'
-  depends_on "protobuf" :version => '>= 3.0.0-beta3',
+  depends_on "protobuf" :version => '>= 3.0.0-beta3'
 
 
   def install
