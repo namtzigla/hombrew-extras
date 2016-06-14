6bdc4f94a77c349556a492353cbde9bdeb491fd7
Florin STAN <namtzigla@users.noreply.github.com>
2016-06-14 16:16:21 -0600

    Update grpc.rb

--- grpc.rb
+++ grpc.rb
@@ -1,1 +1,16 @@
 
+require 'formula'
+
+class Pjsip < Formula
+  homepage 'http://grpc.io'
+  url ' https://github.com/grpc/grpc/archive/release-0_14_1.tar.gz'
+  sha256 '006cde82481d72f3490e7d933fe91a81bec3b30a6928251796ea47a0098267de'
+
+
+  def install
+    ENV.j1
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
