204b66508e0dcc3b1a8011bce098151721055c45
Florin Stan <tom@tcnp3.com>
2017-05-31 09:44:14 -0600

    added pjsip 2.6

--- pjsip@2.5.1.rb
+++ pjsip@2.5.1.rb
@@ -1,10 +1,17 @@
-
 require 'formula'
 
 class Pjsip < Formula
   homepage 'http://www.pjsip.org'
-  url 'http://www.pjsip.org/release/2.6/pjproject-2.6.tar.bz2'
+  url 'http://www.pjsip.org/release/2.5.1/pjproject-2.5.1.tar.bz2'
   sha256 'c5a63bed7a0832ff53ddcd69612cf43148019d0f320b22beb5ca2223bc857dcb'
+
+  # 1. We aren't cross compiling
+  #    pjsip thinks we are, this is fixed somewhere between revision 4305 and
+  #    4621. This should be removed when this formula is updated to 2.2.
+  # 2, 3. Clang compatibility
+  #    This is fixed in revision 4588 and should be removed when this formula
+  #    is updated to 2.2. http://trac.pjsip.org/repos/ticket/1576
+  # patch :DATA
 
   def install
     ENV.j1
