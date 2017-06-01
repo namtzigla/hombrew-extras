65b826751b8af47af9849439ecc35e96f8d2a114
Florin STAN <namtzigla@users.noreply.github.com>
2017-05-31 20:59:13 -0600

    Update pjsip@2.5.1.rb

--- pjsip@2.5.1.rb
+++ pjsip@2.5.1.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Pjsip < Formula
+class PjsipAT251 < Formula
   homepage 'http://www.pjsip.org'
   url 'http://www.pjsip.org/release/2.5.1/pjproject-2.5.1.tar.bz2'
   sha256 'c5a63bed7a0832ff53ddcd69612cf43148019d0f320b22beb5ca2223bc857dcb'
